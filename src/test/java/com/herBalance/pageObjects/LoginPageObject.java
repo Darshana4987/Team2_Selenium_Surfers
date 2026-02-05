79004ab13941a23085c7b6af3ee9ff1aa9035f22
darshana <darshanaadhav@gmail.com>
2026-02-05 14:03:22 -0500

    adding login files

--- src/test/java/com/herBalance/pageObjects/LoginPageObject.java
+++ src/test/java/com/herBalance/pageObjects/LoginPageObject.java
@@ -11,43 +11,107 @@ import java.time.Duration;
 import com.herBalance.utils.ConfigReader;
 
 public class LoginPageObject {
-	
+
 	public WebDriver driver;
 	WebDriverWait wait;
 	private static Logger logger = LogManager.getLogger();
 
+	private By usernameInput = By.name("username");
+	private By passwordInput = By.name("password");
+	private By loginButton = By.xpath("//button[@type='submit' and normalize-space()='LogIn']");
+	private By dashboardHeader = By.xpath("//h1[contains(normalize-space(), 'Dashboard')]");
+	private By errorMessageLocator = By.xpath("//div[@class='error-message' or contains(text(),'Login Failed') ]");
+	private By emptyErrorMsg =By.xpath("//p[contains(text(), 'Email is required') or contains(text(), 'Password is required')]");
+	private By logoHerBalance =By.xpath("//img[@alt='HerBalance Logo']");
+	private By logintabactive =By.xpath("//button[@role='tab' and normalize-space()='Login' and @data-state='active']");
+	private By showPasswordCheckbox = By.xpath("//button[@id='showPassword' and @role='checkbox']");
+	private By passwordfield = By.xpath("//input[@name='password']");
+	private By passwordInputField = By.xpath("//input[@type='password' and @name='password']");
+	
+
 	public LoginPageObject(WebDriver driver) {
 		this.driver = driver;
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
 	public void geturll() {
-
-		// driver = DriverFactory.getDriver(); // your driver setup
-				wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-				driver.get(ConfigReader.getUrl());
-				
+		
+		driver.get(ConfigReader.getUrl());
 	}
-	
+
 	public void getusernamePwd()
 	{
-	WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-	emailInput.sendKeys(ConfigReader.getUsername());
-	WebElement passwordInput = driver.findElement(By.name("password"));
-	passwordInput.sendKeys(ConfigReader.getPassword());
+		wait.until(ExpectedConditions.visibilityOfElementLocated(usernameInput)).sendKeys(ConfigReader.getUsername());
+		wait.until(ExpectedConditions.visibilityOfElementLocated(passwordInput)).sendKeys(ConfigReader.getPassword());
+		
 	}
-	
+
 	public void loginbtn()
 	{
-		WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit' and text()='LogIn']"));
-		loginButton.click();
-	    logger.info("Logging in Her balance application");
-	    
+		wait.until(ExpectedConditions.visibilityOfElementLocated(loginButton)).click();
+		logger.info("Logging in Her balance application");
+
 	}
 
 	public boolean isDashboardLoaded() {
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(
-	        By.xpath("//h1[contains(text(),'Dashboard')]")
-	    ));
-	    return driver.getCurrentUrl().contains("/dashboard");
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(),'Dashboard')]")));
+		return driver.getCurrentUrl().contains("/dashboard");
 	}
+	private By tabByName(String tabName) {
+		
+		return By.xpath("//button[@role='tab' and normalize-space()='" + tabName + "']");
+	}
+
+	public boolean isTabVisible(String tabName) {
+		try {
+			WebElement tab = wait.until(ExpectedConditions.visibilityOfElementLocated(tabByName(tabName)));
+			return tab.isDisplayed();
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	public WebElement verifyLogo() {
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(logoHerBalance));
+	}
+	public boolean isShowPasswordOptionVisible() {
+		try {
+			WebElement showPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(showPasswordCheckbox));
+			return showPassword.isDisplayed();
+		} catch (Exception e) {
+			return false;
+			}
+	}
+	public boolean isPasswordFieldVisibleAndMasked() {
+		try {
+		WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(passwordInputField));
+		return passwordField.isDisplayed()&& "password".equals(passwordField.getAttribute("type"))&& "••••••••".equals(passwordField.getAttribute("placeholder"));
+		} catch (Exception e) {
+		return false;
+		}
+		}
+	
+	public boolean isLoginTabActive() {
+		try {
+		WebElement loginTab = wait.until(ExpectedConditions.visibilityOfElementLocated(logintabactive));
+		return loginTab.isDisplayed();
+		} catch (Exception e) {
+		return false;
+		}
+	}
+	private By emailInputField(String placeholderText) {
+		
+			return By.xpath("//input[@type='email' and @placeholder='" + placeholderText + "']");
+		}
+	public boolean isEmailFieldVisibleWithPlaceholder(String placeholderText) {
+		
+		try {
+			
+		WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(emailInputField(placeholderText)));
+		return emailField.isDisplayed()&& placeholderText.equals(emailField.getAttribute("placeholder"));
+		}
+		catch (Exception e) {
+		return false;
+}
+}
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -2,11 +2,38 @@ Feature: Login Functionality on HerBallance Auth page
  		 As a user
          I want to login with valid credentials
           
-#Background: User clicked the login button on the auth page
+# Non-Functional tests
+  Scenario: Verify the logo is displayed on the login page
+    Given User is on the HerBalance auth page
+    Then User should see the HerBalance logo on the page
 
-Scenario:
-Successful login with valid username and password
+  Scenario: Verify the Login and Sign Up tabs is displayed on the login page
+    Given User is on the HerBalance auth page
+    Then User should be able to see "<Tabname>" in login page
 
+    Examples:
+      | Tabname |
+      | Login   |
+      | Sign Up |
+
+Scenario: Verify Login tab is active by default
+    Given User is on the HerBalance auth page
+    Then Login tab should be active
+
+    Scenario: Verify Email ID input field is visible on login page
+  Given User is on the HerBalance auth page
+  Then Email ID input field with placeholder "you@example.com" should be visible
+
+  Scenario: Verify Password input field with masked entry is visible
+  Given User is on the HerBalance auth page
+  Then Password input field with masked entry should be visible
+
+ Scenario: Verify Show Password option is visible
+  Given User is on the HerBalance auth page
+  Then Show password option should be visible
+
+Scenario: Successful login with valid username and password
 Given User is on the HerBalance auth page
 When User clicks the login button after entering valid username and password
 Then user should be redirected to the dashboard
+
