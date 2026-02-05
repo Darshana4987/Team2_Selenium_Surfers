c837ade0f01c89ee212b861ed4215ed29e485b48
s-yogini <s.yogini@gmail.com>
2026-02-05 00:20:03 -0600

    Committing changes to workout

--- src/test/java/com/herBalance/pageObjects/WorkoutPage.java
+++ src/test/java/com/herBalance/pageObjects/WorkoutPage.java
@@ -1,36 +1,53 @@
 package com.herBalance.pageObjects;
 
-import java.time.Duration;
+import static org.testng.Assert.assertTrue;
 
+import java.time.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+
 public class WorkoutPage {
 	
+	private By workoutButton = By.xpath("//span[text()='Workout']/parent::button"); //a[@href='/workouts']/button");
+	
+	private static Logger logger = LogManager.getLogger();
 	public WebDriver driver;
 	WebDriverWait wait;
-	
+		
 //	@FindBy(xpath=("//button[text()='Generate Workout Plan']"))
 //	WebElement generateWorkoutBtn;
 	
 	public WorkoutPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+
+	public boolean isElementPresent(By locator) {
+	    return !driver.findElements(locator).isEmpty();
+	    //returns true if xpath is not empty, element is present
+	}
+		
 	public void clickWorkoutBtn() {
 		
+		    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.elementToBeClickable(workoutButton)).click();
+			
 		//driver.findElement(WorkoutBtn).click();
-		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		 WebElement workoutBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/workouts']/button")));
+		/* WebElement workoutBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/workouts']/button")));
 		
 		 ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);" , workoutBtn);
 		 ((JavascriptExecutor) driver).executeScript("arguments[0].dispatchEvent(new MouseEvent('click',{bubbles:true}));", workoutBtn);
-		
+		 logger.info("Workout button clicked"); */
+	 
 	}
 	
 	public String getUrl() {
@@ -38,103 +55,107 @@ public class WorkoutPage {
 		return driver.getCurrentUrl();
 	}
 	
-	public void seepageElement(String element, String value) {
+	public void seePageElement(String element, String value) {
+		
 		
 		//driver.findElement(element, value);
-		if (element.equals("header")) {
-			// WebElement header = wait.until(ExpectedConditions.visibilityOfElementLocated(
-			// By.xpath("//h1[text()='"+value+"']")));
-			WebElement header = driver.findElement(By.xpath("//h1[text()='" + value + "']"));
-			System.out.println("Element1:\t" + header.getText());
-			// assertTrue(header.isDisplayed());
-			Assert.assertEquals(header.getText(), value, "Page Header not matching.");
+		if (element.equals("Header1")) {
+			assertTrue(isElementPresent(By.xpath("//h1[text()='" + value + "']")),value+" "+element+" Present ");
 		}
-
-		if (element.equals("link")) {
-			WebElement link = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='text-sm' and text()='" + value + "']")));
-			System.out.println("Element2:\t" + link.getText());
-			Assert.assertEquals(link.getText(), value, "Text not matching.");
+		else if (element.equals("Link")) {
+			assertTrue(isElementPresent(By.xpath("//span[@class='text-sm' and text()='" + value + "']")),value+" "+element+" Present ");
+		} 
+		
+		else if (element.equals("Header2")) {
+			assertTrue(isElementPresent(By.xpath("//h2[text()='" + value + "']")),value+" "+element+" Present ");
 		}
 		
-		if (element.equals("headerDailyWorkout")) {
-			WebElement headerDailyWorkout = driver.findElement(By.xpath("//h2[text()='" + value + "']"));
-			
-			System.out.println("Element3:\t" + headerDailyWorkout.getText());
-			Assert.assertEquals(headerDailyWorkout.getText(), value, "Text not matching.");
+		else if (element.equals("Paragraph")) {
+			assertTrue(isElementPresent(By.xpath("//h2[text()='Daily Workout Plan']/parent::div/p")),value+" "+element+" Present ");			
 		}
 		
-		if (element.equals("paragraphFreshAI")) {
-			WebElement paragraphFreshAI = driver.findElement(By.xpath("//h2[text()='Daily Workout Plan']/parent::div/p"));
-			System.out.println("Element4:\t" + paragraphFreshAI.getText());
-			Assert.assertEquals(paragraphFreshAI.getText(), value, "Text not matching.");
+		else if (element.equals("Header3")) {
+			assertTrue(isElementPresent(By.xpath("//h3[text()='No workout plan found']")),value+" "+element+" Present ");
 		}
-		
-		if (element.equals("noworkoutPlan")) {
-			WebElement noworkoutPlan = driver.findElement(By.xpath("//h3[text()='No workout plan found']"));
-			System.out.println("Element5:\t" + noworkoutPlan.getText());
-			Assert.assertEquals(noworkoutPlan.getText(), value, "Text not matching.");
+		else if (element.equals("Button")) {
+			assertTrue(isElementPresent(By.xpath("//button[text()='Generate Workout Plan']")),value+" "+element+" Present ");
+		}
+		else {
+			Assert.assertEquals(element.toString(), "XYZ", "Undefined Scenario Element.");
 		}
 	}
 
 	public void generateWorkoutPlanBtn() {
 		//generateWorkoutBtn.click();
 		
-		WebElement generateWorkoutBtn = driver.findElement(By.xpath("//button[text()='Generate Workout Plan']"));
-		generateWorkoutBtn.click();
-			
+		try {
+			WebElement generateWorkoutBtn = driver.findElement(By.xpath("//button[text()='Generate Workout Plan']"));
+			System.out.println(" Button Text:\t" + generateWorkoutBtn.getText());
+			generateWorkoutBtn.click();
+			Assert.assertEquals(generateWorkoutBtn.getText(), "Generate Workout Plan", "Text not matching.");
+		} catch (NoSuchElementException e) {
+		    System.out.println("Element not found — handling exception");
+		    assertTrue(true,"Generate Workout Plan Button not Present ");
+		}
+		
+	/*	
+		if (generateWorkoutBtn.isDisplayed()) 
+		{
+		   generateWorkoutBtn.click();
+		} 
+		else 
+		{
+			System.out.println(" Workout PLan already Generated !!");
+			throw new SkipException("Dashboard not found");
+			//return;
+		}
+			*/
 	}
 
 	public void seeSection(String elementTitle, String values) {
 		System.out.println("ElementTitle :\t" + elementTitle);
-		if (elementTitle.equals("headerWorkoutPlan")) {
-			WebElement headerWorkoutPlan = driver.findElement(By.xpath("//h2[text()='Daily Workout Plan']"));
-			System.out.println("ElementTitle1 :\t" + headerWorkoutPlan.getText());
-			Assert.assertEquals(headerWorkoutPlan.getText(), values, "Page Header not matching.");
-			
+		if (elementTitle.equals("Header2")) {
+			assertTrue(isElementPresent(By.xpath("//h2[text()='Daily Workout Plan']")),values+" "+elementTitle+" Present ");
+			System.out.println("ElementTitle2 :\t" + values+" "+elementTitle);
 		}
 		
-		if (elementTitle.equals("button")) {
-			WebElement button = driver.findElement(By.xpath("//button[@role='tab' and @aria-selected='true']"));
-			System.out.println("ElementTitle2 :\t" + button.getText());
-			Assert.assertEquals(button.getText(), values, "Button title not matching.");
+		if (elementTitle.equals("Button1")) {
+			assertTrue(isElementPresent(By.xpath("//button[@role='tab' and @aria-selected='true']")),values+" "+elementTitle+" Present ");
+			System.out.println("ElementTitle3 :\t" + values+" "+elementTitle);
+
 		}
 		
-		if (elementTitle.equals("heading")) {
-			WebElement heading = driver.findElement(By.xpath("//h3[@class='text-lg font-semibold']"));
-			System.out.println("ElementTitle3 :\t" + heading.getText());
-			Assert.assertEquals(heading.getText(), values, "Heading title not matching.");
+		if (elementTitle.equals("Header3")) {
+			assertTrue(isElementPresent(By.xpath("//div[normalize-space()='Luteal']")),values+" "+elementTitle+" Present ");
+			System.out.println("ElementTitle4 :\t" + values+" "+elementTitle);
 		}
 		
-		if (elementTitle.equals("completeButton")) {
-			WebElement completeButton = driver.findElement(By.xpath("//button[contains(text(),'Complete')]"));
-			System.out.println("ElementTitle4 :\t" + completeButton.getText());
-			Assert.assertEquals(completeButton.getText(), values, "Button title not matching.");
+		if (elementTitle.equals("Button2")) {
+			assertTrue(isElementPresent(By.xpath("//button[contains(text(),'Complete')]")),values+" "+elementTitle+" Present ");
+			System.out.println("ElementTitle5 :\t" + values+" "+elementTitle);
 		}
 	
-		if (elementTitle.equals("viewallExercises")) {
-			WebElement viewallExercises = driver.findElement(By.xpath("//button[contains(text(),'View All Exercises')]"));
-			System.out.println("ElementTitle5 :\t" + viewallExercises.getText());
-			Assert.assertEquals(viewallExercises.getText(), values, "Button title not matching.");
+		if (elementTitle.equals("Button3")) {
+			assertTrue(isElementPresent(By.xpath("//button[contains(text(),'View All Exercises')]")),values+" "+elementTitle+" Present ");
+			System.out.println("ElementTitle6 :\t" + values+" "+elementTitle);
 		}
 	
-		if (elementTitle.equals("nextWorkoutButton")) {
-			WebElement nextWorkoutButton = driver.findElement(By.xpath("//button[contains(text(),'Generate Next Work Out')]"));
-			System.out.println("ElementTitle6 :\t" + nextWorkoutButton.getText());
-			Assert.assertEquals(nextWorkoutButton.getText(), values, "Button title not matching.");
+		if (elementTitle.equals("Button4")) {
+			assertTrue(isElementPresent(By.xpath("//button[contains(text(),'Generate Next Work Out')]")),values+" "+elementTitle+" Present ");
+			System.out.println("ElementTitle7 :\t" + values+" "+elementTitle);
 		}
 		
-		if (elementTitle.equals("dailyWorkout")) {
-			WebElement dailyWorkout = driver.findElement(By.xpath("//h3[text()='About Daily Workouts']"));
-			System.out.println("ElementTitle6 :\t" + dailyWorkout.getText());
-			Assert.assertEquals(dailyWorkout.getText(), values, "Text not matching.");
+		if (elementTitle.equals("Header4")) {
+			assertTrue(isElementPresent(By.xpath("//h3[text()='About Daily Workouts']")),values+" "+elementTitle+" Present ");
+			System.out.println("ElementTitle8 :\t" + values+" "+elementTitle);
 		}
 			
 		
 	}
 	
 }
-
-/*	public void loginPage() {
+/* //Login
+	public void loginPage() {
 
 		// driver = DriverFactory.getDriver(); // your driver setup
 				wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -149,4 +170,4 @@ public class WorkoutPage {
 				WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit' and text()='LogIn']"));
 				loginButton.click();
 		        logger.info("Logging in Her balance application");
-	}*/
+	}//Login Ends */
